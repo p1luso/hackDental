dea7806fea79e0684e4b193525335a53124a3f04
Santiago Perez Delfino <santiperezdelfino@gmail.com>
2025-04-19 17:11:40 -0300

    fix: change fontSize in preeFooterFounder

--- src/components/organisms/PreFooterFounder/PreeFooterFounder.jsx
+++ src/components/organisms/PreFooterFounder/PreeFooterFounder.jsx
@@ -6,11 +6,11 @@ import styles from "./styles.module.css";
 const PreFooterFounder = () => {
   return (
     <div className={styles.preefooter}>
-      <Text color="dark" fontFamily="Lexend Medium" fontSize="28px" s={{lineHeight: "1.2"}}>
-      ¿TE INTERESA UNA AUDITORÍA DE MARKETING
+      <Text color="dark" fontFamily="Lexend Medium" fontSize="24px" s={{lineHeight: "1.2"}}>
+      ¿Te interesa una audotoria de marketing
         </Text>
-        <Text type="subtitle" color="dark" bold="semibold" fontFamily="Lexend" s={{lineHeight: "1.2"}} fontSize="32px">
-      100% GRATUITA?
+        <Text type="subtitle" color="dark" bold="semibold" fontFamily="Lexend" s={{lineHeight: "1.2"}} fontSize="28px">
+      100% gratuita?
         </Text>
         <div className={styles.subtitle}>
       <Text bold="font-light" color="dark">
