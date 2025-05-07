89857eb9fa7fac66494d4a147e49a57a8575292d
Santiago Perez Delfino <santiperezdelfino@gmail.com>
2025-05-07 18:15:20 -0300

    .

--- src/pages/OurProgram/OurProgram.jsx
+++ src/pages/OurProgram/OurProgram.jsx
@@ -221,64 +221,6 @@ const OurProgram = () => {
             </Text>
       <DaysCounter targetDate="2025-06-01" />
       </section>
-      <section className={styles.dentalCover}>
-        <div className={styles.dentalContainer}>
-          <div className={styles.contentContainer_dental}>
-            <div className={styles.founderButtonMobile}>
-              <IconTextButton size="100%" colorVariant="primary-darker">
-                Pedir auditoría
-              </IconTextButton>{" "}
-            </div>
-            <div className={styles.imageWrapper}>
-              <img src={CoberturaDental} alt="" />
-            </div>
-
-            <div className={styles.founderText}>
-              <div className={styles.titles}>
-                <Text
-                  type="bigtitle"
-                  textAlign="start"
-                  color="black"
-                  fontFamily="lexend"
-                  bold="font-light"
-                  fontSize="26px"
-                  fontSizeMobile="21px"
-                  s={{ fontWeight: "500" }}
-                >
-                  Dueño de Consultorio Dental
-                </Text>
-                <Text
-                  type="bigtitle"
-                  fontFamily="lexend"
-                  textAlign="start"
-                  color="black"
-                  bold="semi-bold"
-                  fontSize="32px"
-                  fontSizeMobile="24px"
-                  s={{ lineHeight: "1", fontWeight: "600" }}
-                >
-                  ¿Te interesa una auditoría gratuita?
-                </Text>
-              </div>
-              <Text
-                fontFamily="Lexend"
-                color="black-lighter"
-                fontSize="18px"
-                fontSizeMobile="16px"
-                bold="font-light"
-              >
-                Averigua si tu consultorio califica para nuestros servicios
-              </Text>
-
-              <div className={styles.founderButton}>
-                <IconTextButton size="260px" colorVariant="white-green">
-                  Pedir auditoría
-                </IconTextButton>{" "}
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
       <div className={styles.progressBarSection}>
         <div className={styles.progressTexts}>
           <h2 className={styles.progressTitle}>
